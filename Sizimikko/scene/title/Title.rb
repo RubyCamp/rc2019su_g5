51e1b5abc8eea0e277bdb810f426b2517a42d0d9
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-08-20 21:33:51 +0900

    タイトルの文字に変更を加えた
    描画文字、サイズの変更
    サブタイトルを暗転表示するように

--- Sizimikko/scene/title/Title.rb
+++ Sizimikko/scene/title/Title.rb
@@ -4,17 +4,33 @@ require_relative  "../SceneManager.rb"
 class Title
     private 
         @font
+        @subfont
+        @TITLE
+        @SUBTITLE
+        @alpha
     public
     def initialize()
-        @font = Font.new(32)
+        @cycle = 3000.0
+        @twopi = 6.28
+        @font = Font.new(50)
+        @subfont = Font.new(27)
+        @TITLE = "しまねのねこの冒険"
+        @SUBTITLE = "aボタンでゲーム画面へ"
+        @alpha = 0
     end
     def update()
+        changeAlpha()
         if Input.key_push?(K_A) then
             SceneManager.setNextScene(:GAME)
         end
     end
     def draw()
-        Window.draw_font(100,100,"タイトル画面です aボタンでゲーム画面へ", @font)
+        Window.draw_font(Window.width/2-@font.get_width(@TITLE)/2, 200, @TITLE, @font)
+        Window.draw_font(Window.width/2-@subfont.get_width(@SUBTITLE)/2, Window.height - 200,@SUBTITLE, @subfont,{alpha: @alpha})
+    end
+    def changeAlpha()
+        t = Window.running_time;
+        @alpha = (Math.sin(t % @cycle / @cycle * @twopi) * 0.42 + 0.58) * 255;
     end
 
 end

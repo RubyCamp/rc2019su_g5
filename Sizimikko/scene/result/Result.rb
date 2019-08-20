371150aad52bd6ca29ce605721c790cc8e8e0e24
片岡　陸 <kd1242488@st.kobedenshi.ac.jp>
2019-08-20 22:53:09 +0900

    リザルト画面基盤追加

---
+++ Sizimikko/scene/result/Result.rb
@@ -0,0 +1,20 @@
+require "dxruby"
+require_relative  "../Scene.rb"
+require_relative  "../SceneManager.rb"
+
+class Result < SceneIF
+    private
+        @image
+    public
+        def initialize()
+
+        end
+
+        def update()
+
+        end
+
+        def draw()
+
+        end
+end

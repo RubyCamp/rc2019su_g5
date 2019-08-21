bff7ecd44e3ffa6889d42c5ff22af3731ea9a519
kazukifn <oknsdnhrkm56@gmail.com>
2019-08-21 11:59:24 +0900

    障害物の配列に変更

--- Sizimikko/scene/game/ObstacleSet.rb
+++ Sizimikko/scene/game/ObstacleSet.rb
@@ -15,11 +15,20 @@ class ObstacleSet
             if (t - @prevtime >= 3000)
                 @obstacles[@obstacles.size] = Obstacle.new("rock.png")
                 @prevtime = t
-                
             end 
             @obstacles.size.times do |i|           
                 @obstacles[i].update
             end
+            puts(@obstacles)
+
+           
+            @obstacles.size.times do |i| 
+                if(@obstacles[i].offScreencheck() == true) 
+                    @obstacles.shift(1)
+                end
+            end
+                
+
             
             
 

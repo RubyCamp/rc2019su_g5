74a2ed433886c02377a0274b7e65d14017f14b5c
kazukifn <oknsdnhrkm56@gmail.com>
2019-08-21 10:53:56 +0900

    複数障害物が出る

--- Sizimikko/scene/game/Game.rb
+++ Sizimikko/scene/game/Game.rb
@@ -1,24 +1,24 @@
 require "dxruby"
 require_relative  "./Player.rb"
 require_relative  "../Scene.rb"
-require_relative  "./Obstacle.rb"
+require_relative  "./ObstacleSet.rb"
 require_relative  "../SceneManager.rb"
 require_relative  "./Background.rb"
 class Game < SceneIF
     private
         @background #背景のクラス
         @player     #プレイヤークラス 
-        @obstacle   #障害物クラス
+        @obstacleset   #障害物クラス
     public
         def initialize()#初期化処理
             @background = Background.new
             @player = Player.new
-            @obstacle = Obstacle.new("rock.png")
+            @obstacleset = ObstacleSet.new()
         end
         def update()#計算処理
             @background.update()
             @player.update()
-            @obstacle.update()
+            @obstacleset.update()
             if @player === @obstacle#テスト用衝突判定
                 SceneManager.setNextScene(:RESULT)#次フレームでリザルト画面へ移行
             end
@@ -28,6 +28,6 @@ class Game < SceneIF
         def draw()#描画処理
             @background.draw()
             @player.draw()
-            @obstacle.draw()
+            @obstacleset.draw()
         end
 end
--- Sizimikko/scene/game/ObstacleSet.rb
+++ Sizimikko/scene/game/ObstacleSet.rb
@@ -2,17 +2,33 @@ require_relative "./Obstacle.rb"
 
 class ObstacleSet
     private
-        @obstacle#集合をどう管理するかは聞いてみる
+        @obstacles#集合をどう管理するかは聞いてみる
     public
         def initialize()
-            @obstacle = []
+            @obstacles = []
+            @prevtime = Window.running_time;
+            
         end
         def update()
+            @obstacles.unshift()
+            t = Window.running_time;
+            if (t - @prevtime >= 3000)
+                @obstacles[@obstacles.size] = Obstacle.new("rock.png")
+                @prevtime = t
+                
+            end 
+            @obstacles.size.times do |i|           
+                @obstacles[i].update
+            end
+            
+            
 
         end
 
         def draw()
-
+            @obstacles.size.times do |i|           
+                @obstacles[i].draw
+            end
         end
 end
     
